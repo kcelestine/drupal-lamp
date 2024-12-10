675db2342d759e0b33a3ecdabe0bb0622de78a46
Khadijah Celestine <khadijah.celestine@gmail.com>
2024-12-10 14:35:30 -0500

    starting over

--- main.tf
+++ main.tf
@@ -64,7 +64,54 @@ resource "aws_instance" "web_server" {
  
 }
 
+# Create a Security Group for RDS instance
+resource "aws_security_group" "rds_sg" {
+  name        = "rds_sg"
+  description = "Security group for RDS instance"
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/24"]  # Allow EC2 to connect to RDS (adjust subnet CIDR)
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Create an RDS instance (MySQL)
+resource "aws_db_instance" "default" {
+  identifier        = "my-mysql-db"
+  engine            = "mysql"
+  engine_version    = "8.0"
+  instance_class    = "db.t3.micro"  # Use an appropriate size
+  allocated_storage = 20
+  storage_type      = "gp2"
+  username          = "admin"
+  password          = "your-db-password"  # Change this password
+  db_name           = "mydb"
+  skip_final_snapshot = true
+  publicly_accessible = true
+  vpc_security_group_ids   = [aws_security_group.rds_sg.id]
+
+  tags = {
+    Name = "MySQL DB"
+  }
+}
+
  # Outputs the public IP
   output "instance_ip" {
     value = aws_instance.web_server.public_ip
   }
+
+  output "rds_endpoint" {
+  value = aws_db_instance.default.endpoint
+}
+
+#mysql -h my-mysql-db.cxy8ci2k6cu6.us-east-1.rds.amazonaws.com -P 3306 -u admin -p
+#mysql -h db.cxy8ci2k6cu6.us-east-1.rds.amazonaws.com -P 3306 -u admin -p
