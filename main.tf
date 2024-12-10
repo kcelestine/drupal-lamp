04f5d027fc1f470a9df627351ecae431f7e90cb4
Khadijah Celestine <khadijah.celestine@gmail.com>
2024-12-10 15:34:46 -0500

    moved db pass into a variable, fixed ansible script

--- main.tf
+++ main.tf
@@ -76,13 +76,6 @@ resource "aws_security_group" "rds_sg" {
     cidr_blocks = ["0.0.0.0/0"]  # Allow access to RDS from any IP address
   }
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Allow SSH access to RDS from any IP address
-  }
-
   egress {
     from_port   = 0
     to_port     = 0
