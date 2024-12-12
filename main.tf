28f912c5268cf439e9a169cea213c9a0f3272cfc
Khadijah Celestine <khadijah.celestine@gmail.com>
2024-12-12 00:38:48 -0500

    added networking and some variables

--- main.tf
+++ main.tf
@@ -1,24 +1,40 @@
+terraform {
+
+  required_version = "1.10.1"
+
+  required_providers {
+
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.80.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+
+}
+
+provider "random" {}
+
 # Provider Configuration
 provider "aws" {
-  region = "us-east-1"  
+  region = "us-east-1"
 }
 
-# Generate SSH Key Pair for Ansible
-resource "aws_key_pair" "drupal_key" {
-  key_name   = "drupal-key"
-  public_key = file("~/.ssh/id_rsa.pub")  # Make sure you have your public key in this path
-}
-
-# Security Group Configuration
+# Security Group for EC2 Instance
 resource "aws_security_group" "allow_ssh_http" {
-  name = "allow-ssh-http"
+  name        = "allow-ssh-http"
   description = "Allow SSH and HTTP traffic"
+  vpc_id = aws_vpc.main.id
 
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [var.my_ip]
   }
 
   ingress {
@@ -36,44 +52,49 @@ resource "aws_security_group" "allow_ssh_http" {
   }
 }
 
-# EC2 Instance Configuration
-resource "aws_instance" "web_server" {
-  ami             = "ami-0e2c8caa4b6378d8c"  
-  instance_type   = "t2.micro"  
-  key_name        = aws_key_pair.drupal_key.key_name
-  security_groups = [aws_security_group.allow_ssh_http.name]
+# Data Source for Latest Ubuntu 24 AMI
+data "aws_ami" "ubuntu" {
 
-  tags = {
-    Name = "drupal-server"
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["*ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
 
-  # Provisioner to run remote-exec for initial setup
-  provisioner "remote-exec" {
-    inline = [
-      "echo 'Provisioning EC2 instance!'"
-    ]
-
-    connection {
-      type        = "ssh"
-      user        = "ubuntu"
-      private_key = file("~/.ssh/id_rsa")
-      host        = self.public_ip
-    }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
   }
 
- 
+  owners = ["099720109477"]
 }
 
-# Create a Security Group for RDS instance
+# EC2 Instance Configuration
+resource "aws_instance" "web_server" {
+  ami             = data.aws_ami.ubuntu.id
+  instance_type   = var.ec2_instance_type
+  key_name        = var.aws_ec2_key
+  vpc_security_group_ids = [aws_security_group.allow_ssh_http.id]
+  subnet_id       = aws_subnet.public_subnet_1a.id
+
+  tags = {
+    Name = var.ec2_instance_tag
+  }
+}
+
+# Security Group for RDS instance
 resource "aws_security_group" "rds_sg" {
-  name        = "rds_sg"
-  description = "Security group for RDS instance"
+  name        = var.rds_security_group_name
+  description = var.rds_security_group_description
+  vpc_id = aws_vpc.main.id
 
   ingress {
-    from_port   = 3306
-    to_port     = 3306
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Allow access to RDS from any IP address
+    from_port = 3306
+    to_port   = 3306
+    protocol  = "tcp"
+    #cidr_blocks = ["0.0.0.0/0"]  # Allow access to RDS from any IP address
+    security_groups = [aws_security_group.allow_ssh_http.id]
   }
 
   egress {
@@ -84,34 +105,29 @@ resource "aws_security_group" "rds_sg" {
   }
 }
 
+resource "random_id" "unique_id" {
+  byte_length = 8
+}
+
 # Create an RDS instance (MySQL)
 resource "aws_db_instance" "default" {
-  identifier        = "my-mysql-db"
-  engine            = "mysql"
-  engine_version    = "8.0"
-  instance_class    = "db.t3.micro"  # Use an appropriate size
-  allocated_storage = 20
-  storage_type      = "gp2"
-  username          = "admin"
-  password          = var.db_pass  # Use a variable for the password
-  db_name           = "drupal"
-  skip_final_snapshot = true
-  publicly_accessible = true
-  vpc_security_group_ids   = [aws_security_group.rds_sg.id]
+  identifier             = "drupal-db-${random_id.unique_id.hex}"
+  engine                 = "mysql"
+  engine_version         = "8.0"
+  instance_class         = var.rds_instance_type
+  allocated_storage      = 20
+  storage_type           = "gp2"
+  username               = "admin"
+  password               = var.db_pass
+  db_name                = var.rds_db_name
+  skip_final_snapshot    = true
+  publicly_accessible    = false
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+  db_subnet_group_name   = aws_db_subnet_group.drupal_data_subnet_group.name
 
   tags = {
-    Name = "MySQL DB"
+    Name = var.rds_instance_tag
   }
 }
 
-
-
- # Outputs the public IP
-  output "instance_ip" {
-    value = aws_instance.web_server.public_ip
-  }
-
-  output "rds_endpoint" {
-  value = aws_db_instance.default.endpoint
-}
 
--- variables.tf
+++ variables.tf
@@ -1,3 +1,86 @@
+variable "aws_ec2_key" {
+  type = string
+}
+
+variable "my_ip" {
+  type = string
+}
+
+variable "ec2_instance_type" {
+  type    = string
+  default = "t2.micro"
+}
+
+variable "ec2_instance_tag" {
+  type    = string
+  default = "drupal-server"
+}
+
+variable "rds_security_group_name" {
+  type    = string
+  default = "rds-sg"
+}
+
+variable "rds_security_group_description" {
+  type    = string
+  default = "Security group for RDS instance"
+}
+
+variable "rds_instance_type" {
+  type    = string
+  default = "db.t3.micro"
+}
+
+variable "rds_db_name" {
+  type    = string
+  default = "drupal"
+}
+
+variable "rds_instance_tag" {
+  type    = string
+  default = "drupal-db"
+}
+
 variable "db_pass" {
   type = string
+}
+
+variable "vpc_name" {
+  type    = string
+  default = "drupal-vpc"
+}
+
+variable "public_subnet_cidr" {
+  type    = string
+  default = "10.0.1.0/24"
+}
+
+variable "subnet_az" {
+  type    = string
+  default = "us-east-1a"
+}
+
+variable "public_tag" {
+  type    = string
+  default = "public-drupal"
+}
+
+variable "private_subnet_cidr" {
+  type    = string
+  default = "10.0.2.0/24"
+}
+
+variable "private_tag" {
+  type    = string
+  default = "private-drupal"
+}
+
+variable "private_subnet_group" {
+  type    = string
+  default = "private-drupal"
+}
+
+variable "igw_tag" {
+  type    = string
+  default = "drupal-igw"
 }
