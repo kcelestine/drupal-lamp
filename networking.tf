a7642af8277f1e476cac41f89c77c11995137164
Khadijah Celestine <khadijah.celestine@gmail.com>
2024-12-12 01:47:51 -0500

    added second public subnet in 1b

--- networking.tf
+++ networking.tf
@@ -19,6 +19,17 @@ resource "aws_subnet" "public_subnet_1a" {
   }
 }
 
+resource "aws_subnet" "public_subnet_1b" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.4.0/24"
+  availability_zone = "us-east-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = var.public_tag
+  }
+}
+
 resource "aws_subnet" "private_subnet_1a" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.2.0/24"
