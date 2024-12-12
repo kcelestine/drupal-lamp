88d4a1cbe96b2d34ad08c6cd8840fa5164ddf6cd
Khadijah Celestine <khadijah.celestine@gmail.com>
2024-12-12 02:00:04 -0500

    allow ec2 to get traffic on port 80 from elb only

--- networking.tf
+++ networking.tf
@@ -118,4 +118,62 @@ resource "aws_security_group" "alb_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
+
+# Security Group for EC2 Instance
+resource "aws_security_group" "allow_ssh_http" {
+  name        = "allow-ssh-http"
+  description = "Allow SSH and HTTP traffic"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.my_ip]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]
+  }
+
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Security Group for RDS instance
+resource "aws_security_group" "rds_sg" {
+  name        = var.rds_security_group_name
+  description = var.rds_security_group_description
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port = 3306
+    to_port   = 3306
+    protocol  = "tcp"
+    security_groups = [aws_security_group.allow_ssh_http.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
