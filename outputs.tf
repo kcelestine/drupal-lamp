2c2ca6ea98fa856fd57576d11f515d81c2d9e997
Khadijah Celestine <khadijah.celestine@gmail.com>
2024-12-12 02:30:34 -0500

    added alb endpoint to outputs

--- outputs.tf
+++ outputs.tf
@@ -4,5 +4,10 @@ output "instance_ip" {
 }
 
 output "rds_endpoint" {
-  value = aws_db_instance.default.endpoint
+  value = aws_db_instance.drupal_rds.endpoint
+}
+
+output "loadbalancer_endpoint" {
+  value = aws_lb.drupal_alb.dns_name
+  description = "The DNS name of the Application Load Balancer"
 }
